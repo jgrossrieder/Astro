10b79aca6ba3ea5af9106826e09e45dccd1aad6b
Julien <julien@grossrieder.com>
2017-04-05 21:14:57 +0200

    Removing bases components, adding the horoscope component

--- Clients/Astro.Clients.Web/ClientApp/app/app.module.ts
+++ Clients/Astro.Clients.Web/ClientApp/app/app.module.ts
@@ -3,27 +3,21 @@ import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { HoroscopeComponent } from './components/horoscope/horoscope.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        HoroscopeComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '', redirectTo: 'horoscope', pathMatch: 'full' },
+            { path: 'horoscope', component: HoroscopeComponent },
+            { path: '**', redirectTo: 'horoscope' }
         ])
     ]
 })
---
+++ Clients/Astro.Clients.Web/ClientApp/app/components/horoscope/horoscope.component.ts
@@ -0,0 +1,42 @@
+﻿import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'horoscope',
+    templateUrl: './horoscope.component.html'
+})
+export class HoroscopeComponent {
+    public horoscopes: Horoscope[];
+
+    constructor(http: Http) {
+        //This is dirty, have to create a service after.
+        http.get('/api/horoscopes').subscribe(result => {
+            this.horoscopes = result.json() as Horoscope[];
+        });
+    }
+    public iterate(biggest: number): Array<number> {
+        let list: Array<number> = new Array<number>(biggest);
+        for (let i = 1; i <= biggest; i++) {
+            list.push(i);
+        }
+        return list;
+    }
+}
+interface AstroSign {
+    name: string;
+    order: number;
+    start: Date;
+    end: Date;
+}
+interface HoroscopeTopic {
+    title: string;
+    totalStars: number;
+    stars: number;
+}
+
+interface Horoscope {
+    sign: AstroSign;
+    globalText: string;
+    topics: HoroscopeTopic[];
+}
+
